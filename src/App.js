b8a69dd383b1592d17b7065231b861b14fe9c494
Новокрещенов Артем <102524049+pinklama4@users.noreply.github.com>
2024-03-24 22:34:49 +0300

    fix: name path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<Main/>} />
-          <Route path="Рецепты" element={<MyRecipes />} />
+          <Route path="recipes" element={<MyRecipes />} />
         </Routes>
       </BrowserRouter>
     </div>
