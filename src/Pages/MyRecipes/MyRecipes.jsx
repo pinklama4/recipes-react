ee8f4aaad82143f4c483e136596167f3d7dc8cef
Новокрещенов Артем <102524049+pinklama4@users.noreply.github.com>
2024-04-20 14:53:17 +0300

    add: delite

--- src/Pages/MyRecipes/MyRecipes.jsx
+++ src/Pages/MyRecipes/MyRecipes.jsx
@@ -1,29 +1,34 @@
-import React, {useMemo} from 'react';
-import './MyRecipes.scss'
-import {Header} from "../../components/header/Header";
-export function MyRecipes()  {
-    const renderedRecipes = useMemo(() => {
-        const savedRecipes = localStorage.getItem('recipes') || '[]';
-        const formattedSavedRecipes = JSON.parse(savedRecipes);
-
-        if (Array.isArray(formattedSavedRecipes)) {
-            return formattedSavedRecipes.map((item) => (
-                <div key={item.name} className="all-recipes">
-                    <div className="name">{item.name}</div>
-                    <div className="title">{item.value}</div>
-                </div>
-            ));
-        }
-    }, []);
-        return (
-            <div>
-                <Header/>
-
-                <div className="container-recipes">
-                    <h1>Ваши рецепты</h1>
-                    {renderedRecipes}
-                </div>
-            </div>
-        );
-
-}
+// import React, {useCallback, useMemo} from 'react';
+// import './MyRecipes.scss'
+// import {Header} from "../../components/header/Header";
+// export function MyRecipes()  {
+//
+//     const removeRecipes = useCallback((id) => {
+//         const getRecipes = localStorage.getItem('recipes') || '[]';
+//
+//     }, [])
+//     const renderedRecipes = useMemo(() => {
+//         const savedRecipes = localStorage.getItem('recipes') || '[]';
+//         const formattedSavedRecipes = JSON.parse(savedRecipes);
+//
+//         if (Array.isArray(formattedSavedRecipes)) {
+//             return formattedSavedRecipes.map((item) => (
+//                 <div key={item.name} className="all-recipes">
+//                     <div className="name">{item.name}</div>
+//                     <div className="title">{item.value}</div>
+//                     <button>Удалить</button>
+//                 </div>
+//             ));
+//         }
+//     }, []);
+//         return (
+//             <div>
+//                 <Header/>
+//                 <div className="container-recipes">
+//                     <h1>Ваши рецепты</h1>
+//                     {renderedRecipes}
+//                 </div>
+//             </div>
+//         );
+//
+// }
--- src/components/addRecipes/AddRecipes.jsx
+++ src/components/addRecipes/AddRecipes.jsx
@@ -1,19 +1,48 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import './AddRecipes.scss'
+import '../../Pages/MyRecipes/MyRecipes.scss'
+
+import { v4 as uuidv4 } from 'uuid';
+
 export function AddRecipes() {
+
+    const handleRemove = useCallback((id) => {
+        const savedItems = JSON.parse(localStorage.getItem('recipes')) || [];
+        const updatedItems = savedItems.filter(item => item.id !== id);
+        localStorage.setItem('recipes', JSON.stringify(updatedItems));
+    }, []);
+
     const [name, setName] = useState('');
     const [value, setValue] = useState('');
+
     const handleSubmit = useCallback(() => {
-        const savedRecipes = localStorage.getItem('recipes') || '[]';
-        const formattedSavedRecipes = JSON.parse(savedRecipes);
-        localStorage.setItem(
-            'recipes',
-            JSON.stringify([...formattedSavedRecipes, { name, value }]),
-        );
+        const savedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+        const newId = uuidv4();
+        const newRecipe = { id: newId, name, value };
+        localStorage.setItem('recipes', JSON.stringify([...savedRecipes, newRecipe]));
+        setName('');
+        setValue('');
     }, [name, value]);
-    return(
+
+    const renderedRecipes = useMemo(() => {
+        const savedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+        return savedRecipes.map((item) => (
+            <div key={item.id} className="all-recipes"> {}
+                <div className="name">{item.name}</div>
+                <div className="title">{item.value}</div>
+                <form>
+                    <button onClick={() => handleRemove(item.id)}>Удалить</button> {}
+                </form>
+                <button >Выполнено</button>
+            </div>
+        ));
+    }, [handleRemove]);
+
+
+    return (
         <main>
             <div className="new-recipes">
+                <form action="">
                     <div className="header-recipes">
                         <p>Название:</p>
                         <input
@@ -29,11 +58,17 @@ export function AddRecipes() {
                         <input
                             type="text"
                             className="text-recipes"
-                            placeholder='Введите сождержание рецепта'
-                            value={value} onChange={(e) => setValue(e.target.value)}
+                            placeholder='Введите содержание рецепта'
+                            value={value}
+                            onChange={(e) => setValue(e.target.value)}
                         />
                     </div>
                     <button onClick={handleSubmit} className="button-add">Добавить</button>
+                </form>
+            </div>
+            <div className="container-recipes">
+                <h1>Ваши рецепты</h1>
+                {renderedRecipes}
             </div>
         </main>
     );
